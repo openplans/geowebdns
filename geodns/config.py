cf0c4005aad7da07da8798373755f5d355dd2a50
Ian Bicking <ianb@colorstudy.com>
2010-01-14 18:12:40 -0600

    Use the environment config value

--- geodns/config.py
+++ geodns/config.py
@@ -2,15 +2,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-s = 'postgres://'
-if os.environ.get('CONFIG_PG_USER'):
-    s += os.environ['CONFIG_PG_USER']
-    if os.environ.get('CONFIG_PG_PASSWORD'):
-        s += ':' + os.environ['CONFIG_PG_PASSWORD']
-    s += '@'
-s += os.environ.get('CONFIG_PG_HOST') or 'localhost'
-s += '/' + os.environ['CONFIG_PG_DBNAME']
-
-engine = create_engine(s, echo=True)
+engine = create_engine(os.environ['CONFIG_PG_SQLALCHEMY'], echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
