1627c84b4aaf2375dea4bac72584ebbf39491144
Ian Bicking <ianb@colorstudy.com>
2010-01-19 16:07:34 -0600

    Added KML output

--- geodns/wsgiapp.py
+++ geodns/wsgiapp.py
@@ -1,3 +1,5 @@
+import cgi
+import string
 from webob.dec import wsgify
 from webob import exc
 from webob import Response
@@ -6,6 +8,8 @@ from decimal import Decimal
 from geodns.model import Jurisdiction, metadata
 from geodns.config import session, engine
 from geoalchemy import WKTSpatialElement
+from sqlalchemy.sql import expression
+from sqlalchemy.sql import select
 
 def only_get(func):
     def decorated(*args, **kw):
@@ -23,12 +27,14 @@ class Application(object):
     def __call__(self, req):
         if req.path_info == '/api1/types':
             return self.api1_types(req)
+        if req.path_info.startswith('/api1/kml/'):
+            return self.api1_kml(req)
         if req.path_info_peek() == 'api1':
             return self.api1(req)
         if req.path_info == '/.internal/update_fetch':
             return self.update_fetch(req)
         else:
-            return self.not_found(req)
+            raise exc.HTTPNotFound()
 
     @wsgify
     def update_fetch(self, req):
@@ -42,7 +48,7 @@ class Application(object):
     def api1(self, req):
         lat = Decimal(req.GET['lat'])
         long = Decimal(req.GET['long'])
-        result = self.query((lat, long), type=req.GET['type'])
+        result = self.query(req, (lat, long), type=req.GET['type'])
         return Response(
             dumps(result),
             content_type='application/json')
@@ -50,7 +56,6 @@ class Application(object):
     @wsgify
     @only_get
     def api1_types(self, req):
-        from sqlalchemy.sql import select
         s = select([Jurisdiction.type_uri], distinct=True)
         conn = engine.connect()
         s = conn.execute(s)
@@ -61,9 +66,7 @@ class Application(object):
             dumps(results),
             content_type='application/json')
     
-    def query(self, coords, type):
-        from sqlalchemy.sql import expression
-        from sqlalchemy.sql import select
+    def query(self, req, coords, type):
         ## FIXME: this is a long-winded way of doing the select:
         point = "POINT(%s %s)" % (coords[1], coords[0])
         point = WKTSpatialElement(point)
@@ -76,7 +79,36 @@ class Application(object):
             results.append(dict(
                 type=row.type_uri,
                 name=row.name,
-                uri=row.uri))
+                uri=row.uri,
+                kml_uri="%s/api1/kml/%s" % (req.application_url, row['id'])))
         return {'results': results}
-    
-        
+
+    @wsgify
+    @only_get
+    def api1_kml(self, req):
+        assert req.path_info_pop() == 'api1'
+        assert req.path_info_pop() == 'kml'
+        id = int(req.path_info.lstrip('/'))
+        s = select([Jurisdiction.__table__], Jurisdiction.id==id)
+        conn = engine.connect()
+        s = list(conn.execute(s))[0]
+        kml_inner = session.scalar(s.geom.kml)
+        kml = KML_TEMPLATE.substitute(
+            name=cgi.escape(s.name),
+            uri=cgi.escape(s.uri),
+            polygons=kml_inner)
+        return Response(
+            kml,
+            content_type='application/vnd.google-earth.kml+xml')
+
+KML_TEMPLATE = string.Template("""\
+<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2">
+  <Document>
+    <Placemark>
+      <name>${name}</name>
+      <description>${name}: ${uri}</description>
+${polygons}
+    </Placemark>
+  </Document>
+</kml>""")
