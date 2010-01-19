08603a5b19689cae2546168d0f9e0adcd827f6c2
Ian Bicking <ianb@colorstudy.com>
2010-01-19 10:29:59 -0600

    turn off default sqlalchemy echo

--- geodns/config.py
+++ geodns/config.py
@@ -2,6 +2,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(os.environ['CONFIG_PG_SQLALCHEMY'], echo=True)
+engine = create_engine(os.environ['CONFIG_PG_SQLALCHEMY'])
 Session = sessionmaker(bind=engine)
 session = Session()
